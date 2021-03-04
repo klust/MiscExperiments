9a542cf806c14c19739db26666a8c934342d21be
Kurt Lust <kurt.lust@kuleuven.be>
2021-03-04 20:44:39 +0100

    Further completed 2020.12.lua and moved to a different direxctory as it is really the second variant given in README.md

--- EESSI/Modules/EESSI-posix/2020.12.lua
+++ EESSI/Modules/EESSI-posix/2020.12.lua
@@ -1,43 +1,14 @@
 local eessi_version = "2020.12"
 local eessi_root = "/cvmfs/pilot.eessi-hpc.org/"
 
-local archspec_cpu = os.getenv( 'EESSI_ARCHSPEC_CPU' )
-if ( archspec_cpu == nil ) then
-    LmodError( 'EESSI: Please set the environment variable EESSI_ARCHSPEC_CPU to the CPU in your system as determined by archspec' )
-end
-
 --
 -- We do need some packages unfortunately that should be pretty standard.
 --
 local posix = require( 'posix' )
 
--- Discovering which software directories to use:
--- - Option 1: Detect the cpu architecture as returned by archspec cpu 
---   and use a mapping in the module file to map that onto the triple 
---   for the host type. That mapping table could always be generated
---   offline during the building of a new EESSI stack.
--- - Option 1bis: Simply ask system managers who want to provide EESSI
---   through a module to set a certain environment variable which this
---   module then picks up. This can avoid calling external programs
---   that may or may not slow down the loading and unloading of the
---   module.
--- - Option 2:
---     + First discover the CPU family which is enough to intialize
---       the correct version of the compatibility layer. (uname -m)
---     * Then use the compatibilty layer to run the Python script 
---       that determines the version of the software layer.
--- When EESSI starts supporting macos we also need to add the code
--- to detect the OS or detect the OS by testing some typical environment
--- variables or directories, e.g., assume that a system that has
--- /System/Library/AppleUSBDevice is a Mac and otherwise it's a Linux
--- machine.
-
--- Now if we could get the info in the following line elsewhere...
-local archspec_cpu = 'skylake'
--- Alternatively, it might be enough if we could only determine the CPU family
--- as that is enough to initialize the compatibility layer. We may then try to
--- use that one to discover further properties.
--- Having lscpu and awk or grep and sed might be enough.
+--
+-- Some initialisations.
+-- 
 
 -- Table mapping possible values for arcspec_cpu onto the host triple 
 -- (family, vendor, arch) where vendor is omitted in some cases.
@@ -74,31 +45,6 @@ arch_mapping = {
 
 supported_family = { x86_64 = true, ppc64le = true,  aarch64 = true }
 
---
--- Detect whatever we can detect relatively safely
---
-local uname_os =     posix.uname( '%s' )
-local uname_family = posix.uname( '%m' )
-
---
--- Initilze a number of variables with what we already have
---
-
-local eessi_os_type = '' 
-if ( uname_os == 'Linux' ) then
-    eessi_os_type = 'linux'
-elseif ( uanme_os == 'Darwin' ) then
-    eessi_os_type = 'macos'
-else
-    LmodError( 'EESSI: The operating system ' .. uname_os .. ' as reported by uname -s is not supported' )
-end
-if ( not supported_family[uname_family] ) then
-    LmodError( 'EESSI: The processor family ' .. uname_family .. ' as reported by uname -m is not supported' )
-end
-local eessi_cpu_family = arch_mapping[archspec_cpu].family
-local eessi_cpu_vendor = arch_mapping[archspec_cpu].vendor
-local eessi_arch =       arch_mapping[archspec_cpu].arch
-
 local helpstring = string.gsub( [[
 This module enables the EESSI EESSI_VERSION pilot. It is not needed to 
 execute the initialisation scripts after loading this module.
@@ -112,11 +58,54 @@ whatis( whatisstring )
 
 family( "BaseSoftwwareStack" )
 
+--
+-- Detect whatever we can detect relatively safely and test the values.
+--
+local uname_os = posix.uname( '%s' )
+local eessi_os_type 
+if ( uname_os == 'Linux' ) then
+    eessi_os_type = 'linux'
+elseif ( uanme_os == 'Darwin' ) then
+    eessi_os_type = 'macos'
+else
+    LmodError( 'EESSI: The operating system ' .. uname_os .. ' as reported by uname -s is not supported' )
+end
+
+local eessi_cpu_family = posix.uname( '%m' )
+if ( not supported_family[eessi_cpu_family] ) then
+    LmodError( 'EESSI: The processor family ' .. eessi_cpu_family .. ' as reported by uname -m is not supported' )
+end
+
+local archspec_cpu = os.getenv( 'EESSI_ARCHSPEC_CPU' ) 
+if ( mode() == 'load' ) then
+    if ( archspec_cpu == nil ) then
+        LmodMessage( 'EESSI: No CPU architecture given, so the generic ' .. eessi_cpu_family .. ' software stack will be used. ' ..
+                      'Set the environment variable EESSI_ARDHSPEC_CPU to the CPU in your system as determined by archspec to get an optimized software stack.'  )
+        archspec_cpu = eessi_cpu_family
+    elseif ( arch_mapping[archspec_cpu] == nil ) then
+        LmodError( 'EESSI: ' .. archspec_cpu .. ' is an unsupported CPU.' )
+    elseif ( arch_mapping[archspec_cpu].family ~= eessi_cpu_family ) then
+        LmodError( 'EESSI: The CPU ' .. archspec_cpu .. ' does not correspond to the CPU family detected from the OS.' )
+    end
+else
+    -- No need to print warnings if we are not loading the module, and error messages don't make sense either
+    -- as they should not occur unless the user already broke the environment by unsetting variables.
+    if ( archspec_cpu == nil ) then
+        archspec_cpu = eessi_cpu_family
+    end
+end
+
+
 -- The problem with overwriting PS1 is that the variable is not
 -- correctly restored when unloading the module and no prompt appears.
 -- It may be due to a bug in Lmod because pushenv should restore the value
 -- when unloading the module.
 -- pushenv( "PS1", "[EESSI pilot " .. eessi_version .. "] $ " )
+
+-- -----------------------------------------------------------------------------
+--
+-- General initialisation and initialisation of the compatibility layer
+--
 
 setenv( "EESSI_PILOT_VERSION",   eessi_version )
 setenv( "EESSI_PREFIX",          pathJoin( eessi_root, eessi_version ) )
@@ -139,19 +128,38 @@ local detect_command = pathJoin( eprefix, "/usr/bin/python" ) .. ' ' ..
                        pathJoin( eessi_root, eessi_version )
 LmodMessage( 'TODO: Can we now run ' .. detect_command .. ' and capture the output instead of using the full mapping?')
 
--- Set variables for the software layer.
+-- -----------------------------------------------------------------------------
+--
+-- Initialisation of the software layer
+--
+
+--
+-- Determine the the optimal software subdirectory
+-- 
 local eessi_software_subdir
-if ( eessi_cpu_vendor == nil ) then
-    eessi_software_subdir = pathJoin( eessi_cpu_family, eessi_arch )
+if ( arch_mapping[archspec_cpu].vendor == nil ) then
+    eessi_software_subdir = pathJoin( arch_mapping[archspec_cpu].family, arch_mapping[archspec_cpu].arch )
 else
-    eessi_software_subdir = pathJoin( eessi_cpu_family, eessi_cpu_vendor, eessi_arch )
+    eessi_software_subdir = pathJoin( arch_mapping[archspec_cpu].family, arch_mapping[archspec_cpu].vendor, arch_mapping[archspec_cpu].arch )
 end
+
+--
+-- Now do the other initialisations of environment variables.
+-- 
 local eessi_software_path = eessi_root .. eessi_version .. "/software/" .. eessi_software_subdir
+-- Double check that the software path indeed exists. Otherwise there is a problem with the mounts
+-- or the EESSI stack is seriously broken.
+if ( not isDir( eessi_software_patjh ) ) then
+    LmodError(  'EESI: Software directory ' .. eessi_softwarE_path .. ' not found, the EESSI distribution seems to be broken' )
+end
 setenv( "EESSI_SOFTWARE_SUBDIR", eessi_software_subdir )
 setenv( "EESSI_SOFTWARE_PATH",   eessi_software_path )
 
+-- This block still needs refinement as the init script allows for alternative module 
+-- structures.
 setenv( "EESSI_MODULE_PATH",     pathJoin( eessi_software_path, "/modules/all" ) )
 prepend_path( "MODULEPATH",      pathJoin( eessi_software_path, "/modules/all" ) )
+
 -- Set LMOD_RC. This may be a problem if it is already set in the system for other reasons!
 -- We use pushenv to ensure that it is set back to the original value when unloading the module.
 pushenv( "LMOD_RC",               pathJoin( eessi_software_path, "/.lmod/lmodrc.lua" ) )
--- EESSI/Modules/README.md
+++ EESSI/Modules/README.md
@@ -4,6 +4,27 @@
 
   * [EESSI Software Layer issue $68](https://github.com/EESSI/software-layer/issues/68)
 
+## Possible approaches
+
+The main problem is to detect the operating system and the necessary characteristics to determine the
+optimal version of the software stack.
+
+Some options:
+  1. Let the system define two environment variables: one for the OS and one for the CPU
+     architecture as would be returned by ``archspec cpu``. The module file can then contain
+     a mapping table to map that architecture onto the best available stack. This table could
+     be generated offline via a python script that uses archspec and a list of architectures for
+     which EESSI is available. The module can then completely rely on functions that are 
+     guaranteed to be available in Lmod.
+  2. If we allow the use of the Lua module ``posix.uname`` it is easy to determinte the 
+     operating system. One could also determine the processor family, but this is not enough
+     to accurately determine the right software stack. It is only enough to run the generic stack
+     for that family.
+  3. As the steps taken in the previous option are enough to select the right compatibility
+     layer, we can then run a script from the init directory to determine the right version of 
+     the software stack. This requires some more advanced Lua programming to capture the output
+     of a command run in the shell.
+
 ## Some testing of system behaviour
 
 | System                                          | uname -s | uname -m |
@@ -12,4 +33,6 @@
 | Intel Broadwell E5-2680, CentOS 7               | Linux    | x86_64   |
 | AMD zen2 EPYC 7282, CentOS 8                    | Linux    | x86_64   |
 | AMD zen2 EPYC 7742, Cray                        | Linux    | x86_64   |
+
+## Suggestions from others
 
